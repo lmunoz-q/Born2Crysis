c958aab37750efedce680d26e5d0a28e803129d1
LIMachi <lee.mach.hm@gmail.com>
2019-05-07 15:55:30 +0200

    added concept headers

---
+++ inc/doom-nukem.h
@@ -0,0 +1,18 @@
+#ifndef DOOM_NUKEM_H
+# define DOOM_NUKEM_H
+
+#include <libui.h>
+#include <SDL2/SDL.h>
+
+typedef struct	s_e
+{
+	t_libui_window	*win;
+	SDL_Thread		*thread[3];
+}				t_e;
+
+typedef struct	s_player
+{
+
+}				t_player;
+
+#endif
---
+++ inc/player.h
@@ -0,0 +1,39 @@
+#ifndef PLAYER_H
+# define PLAYER_H
+
+#include <>
+#include <SDL2/SDL_stdinc.h>
+
+# define DEFAULT_MAX_SPEED 100.0
+# define DEFAULT_MAX_LIFE 100
+
+# define ACC_GRAVITY 0
+# define ACC_PLAYER_WALK 1
+# define ACC_PLAYER_JUMP 2
+# define ACC_MODIFIER 3
+# define ACCELERATIONS 4
+
+typedef struct	s_player_stats
+{
+	int			life;
+	int			max_life;
+	int			armor;
+	int			stamina;
+	int			max_stamina;
+	int			kill_count;
+	int			score;
+	Uint8		script_data[64];
+}				t_player_stats;
+
+typedef struct	s_player
+{
+	double			pos[3];
+	double			velocity[3];
+	double			acceleration[ACCELERATIONS][3];
+	double			direction[2];
+	int				sector;
+	t_inventory		inventory;
+	t_player_stats	stats;
+}				t_player;
+
+#endif
