c65a2ace3bc2f7678e59154f6ddacfc7755db6ac
Marek FISCHER <mfischer@e2r8p8.42.fr>
2019-05-11 16:30:03 +0200

    tests

--- src/renderer/render.c
+++ src/renderer/render.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 23:28:35 by mfischer          #+#    #+#             */
-/*   Updated: 2019/05/10 23:54:04 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/05/11 16:04:07 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void		render(t_e *e)
 {
-	
+	render_sector(get_sector(e->main_player.sector, &e->world), e->win, e->camera);
 	libui_window_refresh(e->win);
 }
