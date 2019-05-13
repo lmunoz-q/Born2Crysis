020a0a97c4e68a62e88079d6bbb7bfd185dd42f0
Marek FISCHER <mfischer@e2r4p10.42.fr>
2019-05-13 19:20:24 +0200

    fuck this

--- src/camera/init_camera.c
+++ src/camera/init_camera.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 18:15:51 by mfischer          #+#    #+#             */
-/*   Updated: 2019/05/13 16:11:49 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/05/13 19:16:53 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void		init_camera(t_camera *cam, t_vec2i win_size)
 {
 	vec3_clear(cam->pos);
-	cam->pos[2] = -2;
+	cam->pos[2] = -5;
 	cam->pos[0] = 2;
-	cam->pos[1] = 0;
+	cam->pos[1] = 1;
 	vec3_init(cam->view_dir);
 	generate_projection_matrix(cam->projection_matrix, win_size);
 }
