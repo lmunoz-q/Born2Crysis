49053edff3f78b3e874cd57e15089b346e610624
Marek Fischer <marek5886@hotmail.com>
2019-06-05 00:27:43 +0200

    i have a long road ahead

--- src/graphics_new/render_sector.c
+++ src/graphics_new/render_sector.c
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_world.c                                       :+:      :+:    :+:   */
+/*   render_sector.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/04 22:47:05 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/04 23:29:06 by mfischer         ###   ########.fr       */
+/*   Created: 2019/06/04 23:33:23 by mfischer          #+#    #+#             */
+/*   Updated: 2019/06/04 23:54:02 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "world.h"
+#include "graphics.h"
 
-t_bool		init_world(t_world *world)
+void		render_sector(	t_world *world, int sector_id,
+							t_libui_window *win, t_camera *cam)
 {
-	if (!(world->pb = get_buffer(world)))
-		return (FALSE);
+	t_sector	*sector;
+	int			i;
+	
+	i = -1;
+	if (!(sector = get_sector(sector_id, world)))
+		return ;
+	while (++i < sector->polygonnum)
+		//render polygons
+	i = -1;
+	while (++i < sector->objectnum)
+		//render objects
 }
