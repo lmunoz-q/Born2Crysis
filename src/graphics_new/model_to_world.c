fc1392d5286a2aef0aec7fc32cb894dbc1d15751
Marek Fischer <marek5886@hotmail.com>
2019-06-06 00:25:02 +0200

    model to world done

--- src/graphics_new/model_to_world.c
+++ src/graphics_new/model_to_world.c
@@ -6,13 +6,48 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 22:37:58 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/05 22:38:56 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/06/06 00:24:43 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "graphics.h"
 
-model_to_world(t_polygon *polygon, double mat[4][4],double pos[3], t_polygon *p)
+static t_bool	cull_backface(t_polygon *p, double pos[3])
 {
+	double tmp[3];
+	double tmp2[3];
 	
+	vec3vec3_substract(p->v20, p->v01, tmp);
+	vec3vec3_substract(p->v12, p->v01, tmp2);
+	vec3vec3_crossproduct(tmp, tmp2, p->normal);
+	vec3_normalize_s(p->normal);
+	vec3vec3_substract(p->v01, pos, tmp);
+	return ((vec3_dot(p->normal, tmp) > 0) ? TRUE : FALSE);
+}
+
+int			model_to_world(t_mesh *mesh, double pos[3], t_polygon *p)
+{
+	int			num;
+	int			i;
+	t_polygon	tmp;
+
+	i = -1;
+	num = 0;
+	while (++i < mesh->polygonnum)
+	{
+		mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v01, tmp.v01);
+		mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v12, tmp.v12);
+		mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v20, tmp.v20);
+		if (cull_backface(&tmp, pos))
+		{
+			vec2_copy(tmp.v01_uv, mesh->polygons[i].v01_uv);
+			vec2_copy(tmp.v12_uv, mesh->polygons[i].v12_uv);
+			vec2_copy(tmp.v20_uv, mesh->polygons[i].v20_uv);
+			vec3_copy(tmp.v_light, mesh->polygons[i].v_light);
+			tmp.tex_id = mesh->polygons[i].tex_id;
+			p[num] = tmp;
+			num++;
+		}
+	}
+	return (num);
 }
--- src/graphics_new/render_sector.c
+++ src/graphics_new/render_sector.c
@@ -6,20 +6,21 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 23:33:23 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/05 23:17:42 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/06/05 23:26:17 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "graphics.h"
 
-void		render_sector(	t_sector *sector, t_libui_window *win, t_camera *cam)
+void		render_sector(	t_sector *sector, t_camera *cam, SDL_Surface *surface)
 {
-	t_sector	*sector;
-	int			i;
-	
+	int i;
+
 	i = -1;
-	if (!(sector = get_sector(sector_id, world)))
-		return ;
+	while (++i < sector->meshnum)
+		render_mesh();
+	i = -1;
 	while (++i < sector->objectnum)
-		//render objects
+		render_object();
+	//HANDLE PORTALS
 }
