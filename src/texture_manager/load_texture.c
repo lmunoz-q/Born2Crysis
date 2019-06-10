31d684875d29159d0abca2f06dda0eca9a645b19
Marek Fischer <marek5886@hotmail.com>
2019-06-10 16:05:10 +0200

    base texture manager done

---
+++ src/texture_manager/load_texture.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_texture.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 15:14:09 by mfischer          #+#    #+#             */
+/*   Updated: 2019/06/10 16:04:20 by mfischer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "texture_manager.h"
+
+static t_texture	set_texture(t_texture tex)
+{
+	static t_texture	texture = NULL;
+	if (tex != NULL)
+		texture = tex;
+	return (texture);
+}
+
+t_texture			get_current_texture(void)
+{
+	return (set_texture(NULL));
+}
+
+void				load_texture(int id)
+{
+	t_texture tmp;
+
+	tmp = set_texture(NULL);
+	if (tmp && tmp->id == id)
+		return;
+	set_texture(get_texture_from_id(id));
+}
