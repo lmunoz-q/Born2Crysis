cead2e282b39bbe577bbe8a47ab4632bf68a04bb
Marek FISCHER <mfischer@e3r1p10.42.fr>
2019-06-11 20:58:13 +0200

    hey, multiple houses here

--- src/graphics_new/render_sector.c
+++ src/graphics_new/render_sector.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 23:33:23 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/10 20:06:56 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/06/11 20:34:08 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ void		render_sector(	t_sector *sector, t_camera *cam, SDL_Surface *surface)
 	i = -1;
 	while (++i < sector->meshnum)
 		render_mesh(&sector->mesh[i], cam, surface);
-	if (sector->objectnum)
-		render_mesh(sector->objects->mesh, cam, surface);
-/* 	i = -1;
+ 	i = -1;
 	while (++i < sector->objectnum)
-		render_object();
+		render_mesh(sector->objects[i].mesh, cam, surface);
 	//HANDLE PORTALS
-*/
+
 }
