7c8c951527f22dc7f2f15189789353abea0bb726
Marek Fischer <marek5886@hotmail.com>
2019-06-17 22:39:04 +0200

    added simple point lighting following the player

--- src/graphics_new/render_sector.c
+++ src/graphics_new/render_sector.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 23:33:23 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/17 15:16:55 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/06/17 22:34:45 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void		render_sector(t_sector *sector, t_camera *cam, SDL_Surface *surface)
 {
 	int i;
 
+	vec3_copy(sector->lights.lights[0].pos, cam->pos);
 	i = -1;
 	while (++i < sector->meshnum)
 		render_mesh(&sector->mesh[i], cam, surface, &sector->lights);
