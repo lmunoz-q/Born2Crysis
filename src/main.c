f4a92354d2f9f468da0070eb6144fef83040a994
Marek Fischer <marek5886@hotmail.com>
2019-06-18 00:09:50 +0200

    wow that's a fancy master branch

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 13:47:53 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/17 23:00:55 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/06/18 00:08:59 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,20 +63,20 @@ void	init_test_world(t_e *e)
 	vec2_copy(p[3].v20_uv, (double [2]){0, 1});
 	p[3].tex_id = load_texture_from_bmp("assets/house_tex.bmp", TX_REPEAT);
 	tmp = load_obj("assets/house.obj");
-	//tmp2 = load_obj("assets/objects/office.obj");
+	//tmp2 = load_obj("assets/objects/ship.obj");
 	e->world.sectors->objectnum = 3;
 	e->world.sectors->objects = (t_object *)malloc(sizeof(t_object) * 3);
-	mf_memcpy(&e->world.sectors->objects[3], obj_to_object(tmp, "assets/diffuse.bmp"), sizeof(t_object));
+	mf_memcpy(&e->world.sectors->objects[3], obj_to_object(tmp, "assets/objects/ship.bmp"), sizeof(t_object));
 	mf_memcpy(&e->world.sectors->objects[0], obj_to_object(tmp, "assets/house_tex.bmp"), sizeof(t_object));
 	mf_memcpy(&e->world.sectors->objects[1], obj_to_object(tmp, "assets/house_tex.bmp"), sizeof(t_object));
 	mf_memcpy(&e->world.sectors->objects[2], obj_to_object(tmp, "assets/house_tex.bmp"), sizeof(t_object));
 	
-	mat4_scale(e->world.sectors->objects[0].mesh->matrix, 0.1, 0.1, 0.1);
+	mat4_scale(e->world.sectors->objects[0].mesh->matrix, 0.2, 0.2, 0.2);
 	mat4_translate(e->world.sectors->objects[0].mesh->matrix, 0, 0, -100);
-	mat4_scale(e->world.sectors->objects[1].mesh->matrix, 0.1, 0.1, 0.1);
-	mat4_translate(e->world.sectors->objects[1].mesh->matrix, 40, 0, -100);
-	mat4_scale(e->world.sectors->objects[2].mesh->matrix, 0.1, 0.1, 0.1);
-	mat4_translate(e->world.sectors->objects[2].mesh->matrix, 80, 0, -100);
+	mat4_scale(e->world.sectors->objects[1].mesh->matrix, 0.2, 0.2, 0.2);
+	mat4_translate(e->world.sectors->objects[1].mesh->matrix, 80, 0, -100);
+	mat4_scale(e->world.sectors->objects[2].mesh->matrix, 0.2, 0.2, 0.2);
+	mat4_translate(e->world.sectors->objects[2].mesh->matrix, 160, 0, -100);
 
 	//LIGHT TEST
 
@@ -87,7 +87,7 @@ void	init_test_world(t_e *e)
 	e->world.sectors[0].lights.lights[0].pos[1] = 0;
 	e->world.sectors[0].lights.lights[0].pos[2] = -70;
 	e->world.sectors[0].lights.lights[0].pos[3] = 1;
-	e->world.sectors[0].lights.lights[0].intensity = 30;
+	e->world.sectors[0].lights.lights[0].intensity = 70;
 
 }
 
