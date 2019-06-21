7398b40530a5e8b44668356705d0d3bcb92365b3
Marek Fischer <marek5886@hotmail.com>
2019-06-21 21:58:59 +0200

    some optimizations

--- inc/texture_manager.h
+++ inc/texture_manager.h
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 20:44:45 by mfischer          #+#    #+#             */
-/*   Updated: 2019/06/10 19:56:12 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/06/21 20:48:32 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,6 @@ int					load_texture_from_bmp(char *path, t_texture_mode mode);
 t_texture			get_texture_from_id(unsigned int id);
 t_texture			get_current_texture(void);
 void				load_texture(unsigned int id);
-int					texture_get_pixel(int y, int x);
+int					texture_get_pixel(int y, int x, t_texture texture);
 
 #endif
