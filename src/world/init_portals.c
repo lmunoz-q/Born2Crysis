13d135e56b91790f59c839a1ffb901fbe96124f0
Marek FISCHER <mfischer@e1r2p9.42.fr>
2019-07-02 18:11:45 +0200

    added portals@

--- src/world/init_portals.c
+++ src/world/init_portals.c
@@ -6,11 +6,12 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 22:15:53 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/02 16:40:11 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/07/02 17:38:33 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "world.h"
+#include <stdio.h>
 
 void	calculate_portal_normal(t_mesh	*m, double normal[3])
 {
@@ -24,6 +25,10 @@ void	calculate_portal_normal(t_mesh	*m, double normal[3])
 		mat4vec4_multiply(m->matrix, m->polygons->v12, tmp2);
 		mat4vec4_multiply(m->matrix, m->polygons->v20, tmp3);
 		vec3p_get_normal(tmp1, tmp2, tmp3, normal);
+		printf("%f, %f, %f \n", normal[0], normal[1], normal[2]);
+		printf("%f, %f, %f \n", tmp1[0], tmp1[1], tmp1[2]);
+		printf("%f, %f, %f \n", tmp2[0], tmp2[1], tmp2[2]);
+		printf("%f, %f, %f \n", tmp3[0], tmp3[1], tmp3[2]);
 	}
 }
 
