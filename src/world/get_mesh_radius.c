8f604e98a7a06ed92e0711c796fa1847f5c5447a
Marek Fischer <marek5886@hotmail.com>
2019-07-11 19:49:52 +0200

    updated get radius function to take into account resize

--- src/world/get_mesh_radius.c
+++ src/world/get_mesh_radius.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 14:19:56 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/03 14:36:11 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/07/11 19:32:37 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,27 @@ static void		is_further(double d[3], double c[3])
 double			get_mesh_radius(t_mesh *mesh)
 {
 	double	res[3];
+	double	v01[4];
+	double	v12[4];
+	double	v20[4];
 	int		i;
 
 	vec3_clear(res);
 	i = -1;
 	while (++i < mesh->polygonnum)
 	{
-		is_further(mesh->polygons[i].v01, res);
-		is_further(mesh->polygons[i].v12, res);
-		is_further(mesh->polygons[i].v20, res);
+		mesh->polygons[i].v01[3] = 0;
+		mesh->polygons[i].v12[3] = 0;
+		mesh->polygons[i].v20[3] = 0;
+		mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v01, v01);
+		mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v12, v12);
+		mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v20, v20);
+		is_further(v01, res);
+		is_further(v12, res);
+		is_further(v20, res);
+		mesh->polygons[i].v01[3] = 1;
+		mesh->polygons[i].v12[3] = 1;
+		mesh->polygons[i].v20[3] = 1;
 	}
 	return (vec3_magnitude(res));
 }
