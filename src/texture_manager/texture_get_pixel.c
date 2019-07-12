dd59f32155653ad9e8aadd518c3410991ce4406d
Marek Fischer <marek5886@hotmail.com>
2019-07-12 12:00:55 +0200

    added optimized clamp function instead of branched if

--- src/texture_manager/texture_get_pixel.c
+++ src/texture_manager/texture_get_pixel.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 18:48:36 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/11 11:21:24 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/07/12 12:00:39 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,7 @@ int				texture_get_pixel(int y, int x, t_texture texture)
 	if (texture->mode == TX_REPEAT)
 		return (((unsigned int *)texture->texture->pixels)[(texture->size.y
 		- (abs(y) % texture->size.y) - 1) * texture->size.x + (abs(x) % texture->size.x)]);
-	if (x <= 0)
-			x = 1;
-	else if (x > texture->size.x)
-			x = texture->size.x;
-	if (y <= 0)
-			y = 1;
-	else if (y > texture->size.y)
-			y = texture->size.y;
+	x = mf_clamp_int(x, 1, texture->size.x);
+	y = mf_clamp_int(y, 1, texture->size.y);
 	return (((unsigned int *)texture->texture->pixels)[(texture->size.y - y) * texture->size.x + x]);
 }
