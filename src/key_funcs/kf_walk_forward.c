48a606aa6bc685b2121feeff32133bb7c5a2a421
Marek Fischer <marek5886@hotmail.com>
2019-07-13 21:57:24 +0200

    blocked camera and added view independent movement

--- src/key_funcs/kf_walk_forward.c
+++ src/key_funcs/kf_walk_forward.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 12:34:08 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/13 20:44:14 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/07/13 21:56:09 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ void	kf_walk_forward(void *param)
 
 	e = param;
 	vec3_copy(vec, e->main_player.pos);
-	vec3scalar_multiply(e->input_map.mouse.front, 0.2, vec2);
+	vec3scalar_multiply(e->main_player.direction, 0.2, vec2);
 	vec3vec3_substract(vec, vec2, e->main_player.pos);
 }
