4997d24c3a4ac5187161078947763977d4606255
LIMachi <lee.mach.hm@gmail.com>
2019-07-18 15:53:19 +0200

    première version de la fonction qui détecte un point dans un triangle extrudé, dois être testé

--- src/physic/point_in_extruded_wall.c
+++ src/physic/point_in_extruded_wall.c
@@ -57,20 +57,39 @@ int	point_in_extruded_wall(t_double3 point, t_wall wall, t_double2 extrusion, t_
 	double		d;
 	t_double3	p;
 	double		dots[3];
+	double		y;
 
 	(void)extrusion;
 	v = d3_substract(point, wall.center);
+	y = d3_dot_product((t_double3){0, 1, 0}, wall.normal);
+	printf("y det (1 floor, 0 wall, -1 ceiling): %f\n", y);
 	d = d3_dot_product(v, wall.normal);
+	printf("det: %f\n", d);
 	p = d3_substract(point, d3_scale(wall.normal, d));
 	printf("projection: %f %f %f\n", p.x, p.y, p.z);
-	dots[0] = d3_dot_product(d3_substract(point, wall.vertices[0]), d3_substract(wall.vertices[1], wall.vertices[0]));
-	dots[1] = d3_dot_product(d3_substract(point, wall.vertices[1]), d3_substract(wall.vertices[2], wall.vertices[1]));
-	dots[2] = d3_dot_product(d3_substract(point, wall.vertices[2]), d3_substract(wall.vertices[0], wall.vertices[2]));
+	dots[0] = d3_dot_product(d3_substract(p, wall.vertices[0]), d3_substract(wall.vertices[1], wall.vertices[0]));
+	dots[1] = d3_dot_product(d3_substract(p, wall.vertices[1]), d3_substract(wall.vertices[2], wall.vertices[1]));
+	dots[2] = d3_dot_product(d3_substract(p, wall.vertices[2]), d3_substract(wall.vertices[0], wall.vertices[2]));
 	printf("dots: %f %f %f\n", dots[0], dots[1], dots[2]);
 	if ((dots[0] > 0.0 || dots[1] > 0.0 || dots[2] > 0.0)
 			&& (dots[0] < 0.0 || dots[1] < 0.0 || dots[2] < 0.0))
 		return (0);
+	printf("in triangle\n");
+	double smd = (d < 0.0 ? -1.0 : 1.0) * d3_squared_magnitude(d3_substract(p, point));
+	printf("magnitude ^ 2 * direction of p->p' %f\n", smd);
+	double ec = (1.0 - fabs(y)) * extrusion.y;
+	printf("extrusion correction: %f\n", ec);
+	printf("positive extrusion (in front of the wall): %f\n", ec);
+	printf("negative extrusion (behind the wall): %f\n", extrusion.x - ec);
+	if (smd < ec && smd > -(extrusion.x - ec))
+		printf("in extrusion\n");
+	else
+		return (0);
+	printf("correction multiplicator: %f\n", ec - smd);
 	if (correction != NULL)
-		*correction = (t_double3){42, 42, 42}; //still need to be done
+	{
+		*correction = d3_scale(wall.normal, ec - smd);
+		printf("correction: %f %f %f\n", correction->x, correction->y, correction->z);
+	}
 	return (1);
 }
--- src/physic/t_double3_complements.c
+++ src/physic/t_double3_complements.c
@@ -14,3 +14,13 @@ t_double3	d3_scale(t_double3 v, double s)
 {
 	return ((t_double3){.x = v.x * s, .y = v.y * s, .z = v.z * s});
 }
+
+double		d3_squared_magnitude(t_double3 v)
+{
+	return (v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+double		d3_magnitude(t_double3 v)
+{
+	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
+}
