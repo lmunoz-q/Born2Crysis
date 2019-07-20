b0edb8a58ba5647310fa937dfd11a94d783bbaa4
Marek Fischer <marek5886@hotmail.com>
2019-07-20 23:54:08 +0200

    heyy

--- src/world/skybox/skybox_set_pos.c
+++ src/world/skybox/skybox_set_pos.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 18:50:03 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/20 19:52:35 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/07/20 23:52:56 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void				skybox_set_pos(t_object *skybox, double pos[3])
 {
 	mat4_init(skybox->mesh->matrix);
 	mat4_translate(skybox->mesh->matrix, pos[0], pos[1], pos[2]);
-	mat4_rotate_yaw(skybox->mesh->matrix, -90);
-	mat4_scale(skybox->mesh->matrix, 2, 2, 2);
+	//mat4_rotate_yaw(skybox->mesh->matrix, -90);
+	mat4_scale(skybox->mesh->matrix, 100, 100, 100);
 }
