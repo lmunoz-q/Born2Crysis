c2914293013e92874c796b1d787e7075464d2aa0
ZzkilzZ <ZzkilzZ@github.com>
2019-07-22 12:33:56 +0200

    added item definition header

---
+++ inc/item.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   item.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/22 12:05:37 by mfischer          #+#    #+#             */
+/*   Updated: 2019/07/22 12:32:28 by mfischer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITEM_H
+# define ITEM_H
+
+typedef struct		s_item_stats
+{
+	int				attack_dmg;
+	int				armor;
+	int				durability;
+}					t_item_stats;
+
+typedef struct		s_item
+{
+	int				id;
+	char			name[32];
+	t_item_stats	stats;
+	void			(*f)(void *);
+	void			*param;
+	void			*model;
+}					t_item;
+
+#endif
