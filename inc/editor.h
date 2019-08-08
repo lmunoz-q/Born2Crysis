7b2cc88c416705db9e77a1c324e4f1659057f572
wildos <tfernand@student.42.fr>
2019-08-08 17:49:09 +0200

    ajout debut interface

--- inc/editor.h
+++ inc/editor.h
@@ -3,19 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   editor.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tfernand <tfernand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 18:35:47 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/26 18:41:36 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/08 17:42:56 by tfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EDITOR_H
 # define EDITOR_H
 
+#define EDITOR_MENU_WIDTH 500
+
+typedef struct s_editor_interface
+{
+	TTF_Font		*font;
+
+	t_libui_widget  editor_container;
+	t_libui_widget	save_textbutton;
+	t_libui_widget	new_textbutton;
+	t_libui_widget	wall_textbutton;
+	t_libui_widget	obj_textbutton;
+
+	t_libui_widget select_container;
+	t_libui_widget select_label;
+	t_libui_widget selected_file_label;
+} t_editor_interface;
+
 typedef struct		s_editor
 {
 	
 }					t_editor;
+
 
 #endif
