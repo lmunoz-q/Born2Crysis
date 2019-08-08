03d48eb64190fbf51710814eed7ab08d51f3cb3d
LIMachi <lee.mach.hm@gmail.com>
2019-08-08 17:34:13 +0200

    small modification so that when you are on the ground, directly touching a wall or ceiling, the collision is reported with a correction of 0 (to detect continuous contact)

--- src/physic/update_entity.c
+++ src/physic/update_entity.c
@@ -14,47 +14,42 @@ int	update_entity_against_mesh(t_entity *proj, t_world *world, t_entity *ent, t_
 	it = -1;
 	if (mesh->walls == NULL)
 		return (0);
-//	printf("testing %d walls\n", mesh->nb_walls);
 	while (++it < mesh->nb_walls)
 	{
-//		printf("wall: %d\n", it);
 		c = (t_vec4d){.c3 = {.vec3d = mesh->walls[it].center, .w = 1}};
 		c.c3.vec3d = vec3vec3_substract(mat4vec4_multiply(mesh->matrix, c).c3.vec3d, proj->position);
-		if (c.n.x * c.n.x + c.n.y * c.n.y + c.n.z * c.n.z
-				<= mesh->walls[it].radius * mesh->walls[it].radius)
+		if (c.n.x * c.n.x + c.n.y * c.n.y + c.n.z * c.n.z <= mesh->walls[it].radius * mesh->walls[it].radius)
 		{
-//			printf("in radius of wall, normal: %f %f %f, vertices:%f %f %f, %f %f %f, %f %f %f\n", mesh->walls[it].normal.n.x, mesh->walls[it].normal.n.y, mesh->walls[it].normal.n.z, mesh->walls[it].vertices[0].n.x, mesh->walls[it].vertices[0].n.y, mesh->walls[it].vertices[0].n.z, mesh->walls[it].vertices[1].n.x, mesh->walls[it].vertices[1].n.y, mesh->walls[it].vertices[1].n.z, mesh->walls[it].vertices[2].n.x, mesh->walls[it].vertices[2].n.y, mesh->walls[it].vertices[2].n.z);
 			wall.normal = mat4vec4_multiply(mesh->matrix, (t_vec4d){.c3 = {.vec3d = mesh->walls[it].normal}}).c3.vec3d;
 			wall.vertices[0] = mat4vec4_multiply(mesh->matrix, (t_vec4d){.c3 = {mesh->walls[it].vertices[0], 1}}).c3.vec3d;
 			wall.vertices[1] = mat4vec4_multiply(mesh->matrix, (t_vec4d){.c3 = {mesh->walls[it].vertices[1], 1}}).c3.vec3d;
 			wall.vertices[2] = mat4vec4_multiply(mesh->matrix, (t_vec4d){.c3 = {mesh->walls[it].vertices[2], 1}}).c3.vec3d;
 			wall.center = mat4vec4_multiply(mesh->matrix, (t_vec4d){.c3 = {mesh->walls[it].center, 1}}).c3.vec3d;
 			wall.radius = mesh->walls[it].radius;
-//			printf("  after transform, normal: %f %f %f, vertices:%f %f %f, %f %f %f, %f %f %f\n", wall.normal.n.x, wall.normal.n.y, wall.normal.n.z, wall.vertices[0].n.x, wall.vertices[0].n.y, wall.vertices[0].n.z, wall.vertices[1].n.x, wall.vertices[1].n.y, wall.vertices[1].n.z, wall.vertices[2].n.x, wall.vertices[2].n.y, wall.vertices[2].n.z);
 			if ((type = entity_wall_collision(*ent, *proj, wall, &cor)))
 			{
-				// printf("type: %d\n", type);
-				// printf("\ncollision found\n\n");
 				proj->position = vec3vec3_add(proj->position, vec3scalar_multiply(mesh->walls[it].normal, cor));
 				if (type == 1)
 				{
-					proj->velocity.n.y = 0;
+					printf("ground contact\n");
+					proj->velocity.n.y = -(D_GRAVITY * DELTATIME);
 					proj->onground = TRUE;
 				}
 				if (type == 2)
 				{
-					// proj->velocity.n.y += (D_GRAVITY * DELTATIME);
 					proj->onground = TRUE;
 				}
 				if (type == 4)
 				{
-					if (proj->velocity.n.y > 0.0)
-						proj->velocity.n.y = 0.0;
+					printf("wall contact\n");
+					// if (proj->velocity.n.y > 0.0)
+					// 	proj->velocity.n.y = 0.0;
 				}
 				if (type == 5)
 				{
-					if (proj->velocity.n.y > 0.0)
-						proj->velocity.n.y = 0.0;
+					printf("wall contact\n");
+					// if (proj->velocity.n.y > 0.0)
+					// 	proj->velocity.n.y = 0.0;
 				}
 			}
 		}
@@ -64,14 +59,12 @@ int	update_entity_against_mesh(t_entity *proj, t_world *world, t_entity *ent, t_
 
 t_entity	basic_physics(t_entity e)
 {
-	// printf("base velocity: %f %f %f\n", e.velocity.n.x, e.velocity.n.y, e.velocity.n.z);
 	e.position = vec3vec3_add(e.position, e.velocity);
 	e.velocity.n.y -= (D_GRAVITY * DELTATIME);
 	if (e.velocity.n.y < -2.0)
 		e.velocity.n.y = -2.0;
 	e.velocity.n.x *= DEFAULT_FRICTION;
 	e.velocity.n.z *= DEFAULT_FRICTION;
-	// printf("new velocity: %f %f %f\n", e.velocity.n.x, e.velocity.n.y, e.velocity.n.z);
 	return (e);
 }
 
@@ -84,19 +77,14 @@ int	update_entity(t_world *world, t_entity *ent)
 
 	if ((sector = get_sector(ent->sector, world)) == NULL)
 		return (0);
-	proj = basic_physics(*ent);/*vec3vec3_add(ent->position, vec3scalar_multiply(ent->velocity, DELTATIME));*/
+	proj = basic_physics(*ent);
 	or = proj.position;
-	// printf("projected position: %f %f %f\n", or.n.x, or.n.y, or.n.z);
 	it = -1;
 	while (++it < sector->objectnum)
 		update_entity_against_mesh(&proj, world, ent, sector->objects[it].mesh);
 	it = -1;
 	while (++it < sector->meshnum)
-	{
-//		printf("mesh: %d\n", it);
 		update_entity_against_mesh(&proj, world, ent, &sector->mesh[it]);
-	}
 	*ent = proj;
-	// ent->position = or;
 	return (proj.position.n.x != or.n.x || proj.position.n.y != or.n.y || proj.position.n.z != or.n.z);
 }
--- src/player/player_update.c
+++ src/player/player_update.c
@@ -12,44 +12,13 @@
 
 #include "doom-nukem.h"
 
-void		player_apply_basic_physics(t_e *e)
-{
-	double	ratio;
-
-	e->main_player.max_speed = (e->main_player.is_running) ? DEFAULT_MAX_RUN_SPEED : DEFAULT_MAX_WALK_SPEED;
-	e->main_player.entity.feet.position = vec3vec3_add(e->main_player.entity.feet.position, e->main_player.entity.feet.velocity);
-	if (e->main_player.entity.feet.position.a[1] < 0)
-	{
-		e->main_player.entity.feet.onground = TRUE;
-		e->main_player.entity.feet.velocity.a[1] = 0;
-	}
-	if (!e->main_player.entity.feet.onground)
-		e->main_player.entity.feet.velocity.a[1] -= (D_GRAVITY * DELTATIME);
-	else
-	{
-		e->main_player.entity.feet.velocity.a[0] *= DEFAULT_FRICTION;
-		e->main_player.entity.feet.velocity.a[2] *= DEFAULT_FRICTION;
-	}
-	if (vec3_magnitude(e->main_player.entity.feet.velocity) > e->main_player.max_speed && e->main_player.entity.feet.onground)
-	{
-		ratio = e->main_player.max_speed / vec3_magnitude(e->main_player.entity.feet.velocity);
-		e->main_player.entity.feet.velocity.a[0] *= ratio;
-		e->main_player.entity.feet.velocity.a[2] *= ratio;
-	}
-}
-
 void			player_update(t_e *e)
 {
 	e->main_player.entity.feet.onground = FALSE;
 	e->main_player.entity.feet.look.a[0] = cos(e->input_map.mouse.pos.n.x * M_PI / 180.0);
 	e->main_player.entity.feet.look.a[1] = 0;
 	e->main_player.entity.feet.look.a[2] = sin(e->input_map.mouse.pos.n.x * M_PI / 180.0);
-	update_player_sector(&e->main_player, &e->world);
-//	player_apply_basic_physics(e);
-	if (update_entity(&e->world, &e->main_player.entity.feet))
-	// {
-		// e->main_player.entity.feet.velocity.n.y = 0;
-//		printf("**** COLLISION *****\n");
-// 	}
+	update_entity(&e->world, &e->main_player.entity.feet);
 	e->main_player.is_running = FALSE;
+	update_player_sector(&e->main_player, &e->world);
 }
