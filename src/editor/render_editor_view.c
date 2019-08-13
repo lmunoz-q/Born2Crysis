b9e8611ecc8c0c409741f9d58cc979d53ffa2e25
Marek Fischer <marek5886@hotmail.com>
2019-08-13 21:11:31 +0200

    put skybox loading in test main and added rendering functions for a distance based rendering. Added the new rendering method to the editor view. TODO: add the old input manager and make a new key binding map

---
+++ src/editor/render_editor_view.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_editor_view.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/13 20:55:16 by mfischer          #+#    #+#             */
+/*   Updated: 2019/08/13 21:02:05 by mfischer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "editor.h"
+
+void 	render_editor_view(t_world *world, t_editor_interface *ei)
+{
+	t_vec3d		look_dir;
+
+	look_dir = vec3vec3_add(ei->editor_cam.pos, ei->editor_cam.view_dir);
+	mf_memset(get_zbuff(), 0, ei->view_container.texture->w * ei->view_container.texture->h * sizeof(double));
+	ei->editor_cam.view_matrix = look_at(ei->editor_cam.pos, look_dir, (t_vec3d){.a = {0, -1, 0}});
+	openworld_render(world, &ei->editor_cam, ei->view_container.texture);
+	skybox_set_pos(world->skybox, ei->editor_cam.pos);
+	render_mesh(world->skybox->mesh, &ei->editor_cam, ei->view_container.texture, NULL);
+	draw_transparent(ei->view_container.texture);
+}
