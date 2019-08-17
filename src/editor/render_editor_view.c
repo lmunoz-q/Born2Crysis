ea2864a0d1e0d3d56962d75d580b75860b3b21ee
Marek Fischer <marek5886@hotmail.com>
2019-08-17 13:58:59 +0200

    memory overwrite is causing the bugs, nothing else could be the issue

--- src/editor/render_editor_view.c
+++ src/editor/render_editor_view.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 20:55:16 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/14 18:35:22 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/17 13:50:24 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ void 	render_editor_view(t_world *world, t_editor_interface *ei)
 	ei->editor_cam.view_matrix = look_at(ei->editor_cam.pos, look_dir, (t_vec3d){.a = {0, -1, 0}});
 	openworld_render(world, &ei->editor_cam, ei->view_container.texture);
 	skybox_set_pos(world->skybox, ei->editor_cam.pos);
-	render_object(world->skybox, &ei->editor_cam, ei->view_container.texture, NULL);
+	render_mesh(world->skybox->mesh, &ei->editor_cam, ei->view_container.texture, NULL);
 	draw_transparent(ei->view_container.texture);
 }
