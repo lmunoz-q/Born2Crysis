2586dcaeaa174cd65ea6584d833f1e4b653fd620
Marek Fischer <marek5886@hotmail.com>
2019-08-17 23:17:15 +0200

    added more generic preview options. item_placer is now the place holder for meshes or objects to be previewed and/or placed

--- src/editor/render_mesh_preview.c
+++ src/editor/render_mesh_preview.c
@@ -1,30 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   render_object_preview.c                            :+:      :+:    :+:   */
+/*   render_mesh_preview.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/12 00:25:41 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/13 17:15:27 by mfischer         ###   ########.fr       */
+/*   Created: 2019/08/17 15:01:26 by mfischer          #+#    #+#             */
+/*   Updated: 2019/08/17 15:48:46 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "editor.h"
 
-void		render_object_preview(t_object *obj, SDL_Surface *surf, t_vec2i size)
+void		render_preview(t_mesh *mesh, SDL_Surface *surf, t_vec2i size)
 {
 	t_camera		camera;
 	
 	vec3_clear(&camera.pos);
 	mf_memset(surf->pixels, 0, surf->w * surf->h * sizeof(Uint32));
-	camera.pos.n.z = 4;
+	camera.pos.n.z = mesh->radius;
 	camera.pos.n.y = 2;
-	camera.view_dir = (t_vec3d){.a = {0, 2, (4) + 1.0}};
-	obj->mesh[0].matrix = mat4_rotate_pitch(obj->mesh[0].matrix, 1);
+	camera.view_dir = (t_vec3d){.a = {0, 2, camera.pos.n.z + 1.0}};
+	mesh->matrix = mat4_rotate_pitch(mesh->matrix, 1);
 	generate_projection_matrix(&camera.projection_matrix, size);
 	mat4_init(&camera.view_matrix);
 	camera.view_matrix = look_at(camera.pos,
 	camera.view_dir, (t_vec3d){.a = {0, -1, 0}});
-	render_object(obj, &camera, surf, NULL);
+	render_mesh(mesh, &camera, surf, NULL);
 }
