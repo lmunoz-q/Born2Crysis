ec621fa211e82f2e5f8cff82c12199e32c10eea1
Marek Fischer <marek5886@hotmail.com>
2019-08-18 16:12:04 +0200

    added roll rotation

--- inc/key_funcs.h
+++ inc/key_funcs.h
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 19:40:54 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/18 16:04:45 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/18 16:09:32 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,7 @@ void	kf_preview_rotate_yaw_r(void *param);
 void	kf_preview_rotate_yaw_l(void *param);
 void	kf_preview_rotate_pitch_r(void *param);
 void	kf_preview_rotate_pitch_l(void *param);
+void	kf_preview_rotate_roll_r(void *param);
+void	kf_preview_rotate_roll_l(void *param);
 
 #endif
