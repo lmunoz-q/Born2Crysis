204b3f5968e3a44322572d663d4af7574438c5ec
Marek Fischer <marek5886@hotmail.com>
2019-08-18 21:43:05 +0200

    light changes

--- src/editor/editor_render.c
+++ src/editor/editor_render.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/14 14:17:35 by tfernand          #+#    #+#             */
-/*   Updated: 2019/08/18 20:46:05 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/18 21:17:34 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void editor_render(t_e *e, t_libui_widgets_surface *ws,
 	mf_memset(ws->surface->pixels, 0,
 			  e->win->surface->pitch * e->win->surface->h);
 	editor_interface->editor_cam.view_dir = e->input_map.mouse.front;
+	remplir_preview(editor_interface, e);
 	remplir_3dview(editor_interface, e);
 	mf_memset(get_zbuff(), 0,
 			  e->win->surface->w * e->win->surface->h * sizeof(double));
-	remplir_preview(editor_interface, e);
 	libui_window_update(e->win);
 }
