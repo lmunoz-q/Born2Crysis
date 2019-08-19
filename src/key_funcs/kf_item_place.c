e96b61e7844ad11ed827ace529134272cd92a047
Marek Fischer <marek5886@hotmail.com>
2019-08-19 11:14:57 +0200

    small fixes

--- src/key_funcs/kf_item_place.c
+++ src/key_funcs/kf_item_place.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/18 16:49:19 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/19 00:26:08 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/19 11:13:10 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,7 @@ void		kf_item_place(void *param)
 		world_add_object(object2, &e->world, e->editor.secteur_courant);
 	}
 	else
-		world_add_mesh(e->editor.item_placer, &e->world, e->editor.secteur_courant);
+	{
+		world_add_mesh(mesh_copy(e->editor.item_placer), &e->world, e->editor.secteur_courant);
+	}
 }
