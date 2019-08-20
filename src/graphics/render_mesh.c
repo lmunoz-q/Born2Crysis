07112c48abbbb1b4a9cdd84b4a0ff5bffa62e010
Marek FISCHER <mfischer@e2r12p7.42.fr>
2019-08-20 17:11:05 +0200

    got rid of objects and made the polybon buffer resize automatically when needed

--- src/graphics/render_mesh.c
+++ src/graphics/render_mesh.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 23:43:13 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/17 13:41:22 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/20 17:10:17 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void		render_mesh(t_mesh *mesh, t_camera *cam, SDL_Surface *surface, t_light_com
 	t_sector	*tmp;
 	t_polygon	*p;
 	
+	if (mesh->polygonnum * 2 > get_polygon_buffer_size())
+		buffer_increase(mesh->polygonnum * 2);
 	p = get_polygon_buffer();
 	count = model_to_world(mesh, (t_vec4d){.c3 = {.vec3d = cam->pos}}, p);
 	if (mesh->sector_id != -1 && count != 0)
