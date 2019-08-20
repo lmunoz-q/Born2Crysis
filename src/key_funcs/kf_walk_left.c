f8be4006cb3f0fb956269455399ef01b3008afd5
Marek Fischer <marek5886@hotmail.com>
2019-08-21 01:05:18 +0200

    gcc errors, var not used

--- src/key_funcs/kf_walk_left.c
+++ src/key_funcs/kf_walk_left.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 13:29:02 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/20 16:28:17 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/21 01:00:33 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	kf_walk_left(void *param)
 
 	e = param;
 	a = e->main_player.entity.body.velocity;
+	(void)a;
 	b = e->main_player.entity.body.look;
 	e->main_player.entity.body.velocity = entity_accelerate(e->main_player.entity.body, vec3scalar_multiply(vec3_normalize(vec3vec3_crossproduct(b, (t_vec3d){{0, 1, 0}})), ACC_PLAYER_WALK));
 }
--- src/key_funcs/kf_walk_right.c
+++ src/key_funcs/kf_walk_right.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 13:31:53 by mfischer          #+#    #+#             */
-/*   Updated: 2019/07/20 16:28:24 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/21 01:00:49 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	kf_walk_right(void *param)
 	union u_vec3d	b;
 
 	e = param;
+	(void)a;
 	a = e->main_player.entity.body.velocity;
 	b = e->main_player.entity.body.look;
 	e->main_player.entity.body.velocity = entity_accelerate(e->main_player.entity.body, vec3scalar_multiply(vec3_normalize(vec3vec3_crossproduct(b, (t_vec3d){{0, 1, 0}})), -ACC_PLAYER_WALK));
