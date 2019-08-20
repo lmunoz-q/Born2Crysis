20f2a1b664204a110faa2a606617e525575c0ebc
wildos <tfernand@student.42.fr>
2019-08-20 15:52:52 +0200

    ajout bouton PORTAIL

---
+++ src/editor/portail_pressed.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   portail_pressed.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfernand <tfernand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/20 15:50:25 by tfernand          #+#    #+#             */
+/*   Updated: 2019/08/20 15:52:15 by tfernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "editor.h"
+
+int portail_pressed(SDL_Event *event, t_libui_widget *widget, void *data)
+{
+	(void)event;
+	(void)widget;
+	(void)data;
+	return (0);
+}
