605eecab807c9c8ced2db5bfc91b82103d1a0b75
Marek Fischer <marek5886@hotmail.com>
2019-08-22 16:47:53 +0200

    you can now place and delete light

--- src/key_funcs/kf_item_place.c
+++ src/key_funcs/kf_item_place.c
@@ -6,16 +6,18 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/18 16:49:19 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/22 14:20:41 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/22 16:43:32 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "key_funcs.h"
 
-static void	make_portal(t_e	*e, t_mesh *mesh)
+static void	make_portal(t_e	*e)
 {
 	int			i;
+	t_mesh		*mesh;
 
+	mesh = mesh_copy(e->editor.item_placer);
 	i = -1;
 	while (++i < mesh->polygonnum)
 	{
@@ -31,7 +33,7 @@ static void	make_light(t_e *e, t_mesh *mesh)
 	t_light		light;
 	t_sector	*tmp;
 
-	light.pos_o = (t_vec4d){.a = {0, 0, 0, 0}};
+	light.pos_o = (t_vec4d){.a = {0, 0, 0, 1}};
 	light.mat = mesh->matrix;
 	light.dir = (t_vec3d){.a = {1, 0, 0}};
 	light.fallof = e->editor.lux_fallof;
@@ -39,7 +41,10 @@ static void	make_light(t_e *e, t_mesh *mesh)
 	light.type = e->editor.lux_type;
 	tmp = get_sector( e->editor.secteur_courant, &e->world);
 	if (tmp)
+	{
 		light_add(&tmp->lights, light);
+		mesh->light_id = tmp->lights.lights[tmp->lights.light_count - 1].id;
+	}
 }
 
 void		kf_item_place(void *param)
@@ -56,11 +61,10 @@ void		kf_item_place(void *param)
 		mesh = mesh_copy(e->editor.item_placer);
 		if (e->editor.is_making_portail)
 			mesh->sector_id = e->editor.secteur2_courant;
-		world_add_mesh(mesh, &e->world, e->editor.secteur_courant);
-		mesh = mesh_copy(e->editor.item_placer);
-		if (e->editor.is_making_portail)
-			make_portal(e, mesh);
 		if (e->editor.is_light)
 			make_light(e, mesh);
+		world_add_mesh(mesh, &e->world, e->editor.secteur_courant);
+		if (e->editor.is_making_portail)
+			make_portal(e);
 	}
 }
