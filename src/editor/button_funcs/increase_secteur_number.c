93d3d81b5ed3464bc1b61124564a2c5fadb2e826
Marek Fischer <marek5886@hotmail.com>
2019-08-22 23:13:46 +0200

    fuuuck

--- src/editor/button_funcs/increase_secteur_number.c
+++ src/editor/button_funcs/increase_secteur_number.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   increase_secteur_number.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfernand <tfernand@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 15:27:06 by tfernand          #+#    #+#             */
-/*   Updated: 2019/08/21 16:40:57 by tfernand         ###   ########.fr       */
+/*   Updated: 2019/08/22 23:04:48 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 int	increase_secteur_number(SDL_Event *event, t_libui_widget *widget,
 							void *data)
 {
+	t_e					*e;
 	t_editor_interface	*editor_interface;
 
 	(void)event;
 	(void)widget;
-	editor_interface = (t_editor_interface *)data;
+	e = data;
+	editor_interface = &((t_e *)data)->editor;
 	if (editor_interface->secteur_courant < MAX_SECTEURS)
 		editor_interface->secteur_courant += 1;
+	if (editor_interface->secteur_courant > e->world.sectornum)
+		sector_create(&e->world);
 	update_secteur_courant_text(&(editor_interface->secteur_selec_label),
 								editor_interface->secteur_courant);
 	return (0);
@@ -30,13 +34,17 @@ int	increase_secteur_number(SDL_Event *event, t_libui_widget *widget,
 int increase_secteur2_number(SDL_Event *event, t_libui_widget *widget,
 							void *data)
 {
-	t_editor_interface *editor_interface;
+	t_e					*e;
+	t_editor_interface	*editor_interface;
 
 	(void)event;
 	(void)widget;
-	editor_interface = (t_editor_interface *)data;
+	e = data;
+	editor_interface = &e->editor;
 	if (editor_interface->secteur2_courant < MAX_SECTEURS)
 		editor_interface->secteur2_courant += 1;
+	if (editor_interface->secteur2_courant > e->world.sectornum)
+		sector_create(&e->world);
 	update_secteur2_courant_text(&(editor_interface->secteur2_selec_label),
 								editor_interface->secteur2_courant);
 	return (0);
