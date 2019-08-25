5703fdfa473998f3092a9b0a65dae649461c8792
Marek FISCHER <mfischer@e1r9p4.42.fr>
2019-08-25 19:42:33 +0200

    fml

--- src/world/mesh_add_physics.c
+++ src/world/mesh_add_physics.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/23 22:05:24 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/25 18:51:31 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/25 19:21:58 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,12 @@ void	mesh_add_physics(t_mesh *mesh)
 	{
 		tmp = mesh->polygons[i];
 		mesh->walls[i] = polygon_to_wall(tmp, mesh->matrix);
-		mesh->walls[i].friction = 0.4;
-		if (vec3_dot(vec3_normalize(vec3p_get_normal(mesh->polygons[i].v01.c3.vec3d, mesh->polygons[i].v12.c3.vec3d, mesh->polygons[i].v20.c3.vec3d)), (t_vec3d){.a = {0, 1, 0}}) > GROUND_RATIO)
+		mesh->walls[i].friction = 0.2;
+		if (vec3_dot(vec3_normalize(vec3p_get_normal(mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v01).c3.vec3d,
+													mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v12).c3.vec3d,
+													mat4vec4_multiply(mesh->matrix, mesh->polygons[i].v20).c3.vec3d)), (t_vec3d){.a = {0, 1, 0}}) > GROUND_RATIO)
 			mesh->walls[i].on_contact_trigger = EFF_RESET_JUMP;
+		else
+			mesh->walls[i].friction = 0.9;
 	}
 }
