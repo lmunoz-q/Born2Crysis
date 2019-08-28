04cf5497cb0656b23b4f9b42650fda680ca4bf48
Marek FISCHER <mfischer@e1r9p17.42.fr>
2019-08-28 12:31:23 +0200

    normal of some physics meshes may return NAN, they will now be ignored

--- src/logic/update.c
+++ src/logic/update.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 19:17:02 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/28 01:40:05 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/08/28 12:16:42 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,4 @@ void		update(t_e *e)
 	player_update(e);
 	camera_update(e);
 	win_condition(e);
-	printf("pos: x.%f y.%f z.%f", e->main_player.entity.body.position.n.x, e->main_player.entity.body.position.n.y, e->main_player.entity.body.position.n.z);
 }
