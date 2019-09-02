02245b29080dc114d8f17114e0eb5c28de69cfc7
Marek Fischer <marek5886@hotmail.com>
2019-09-02 23:57:26 +0200

    added ability to make small movements by using the keypad, no rotations available as for now...

--- src/key_funcs/kf_preview_rotate_yaw.c
+++ src/key_funcs/kf_preview_rotate_yaw.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/18 15:56:18 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/27 17:06:28 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/09/02 23:50:40 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,18 @@ void		kf_preview_rotate_yaw_r(void *param)
 	t_vec4d	rot;
 
 	e = param;
-	rot = quat_rotator((t_vec3d){{1, 0, 0}}, ROTATE_SPEED);
-	e->editor.item_rotation = quat_multiply(rot, e->editor.item_rotation);
+	if (!e->editor.item_placer)
+		return ;
+	if (e->editor.is_modified)
+	{
+		e->editor.item_placer->matrix = mat4_translate(
+			e->editor.item_placer->matrix, ROTATE_SPEED, 0, 0);
+	}
+	else
+	{
+		rot = quat_rotator((t_vec3d){{1, 0, 0}}, ROTATE_SPEED);
+		e->editor.item_rotation = quat_multiply(rot, e->editor.item_rotation);
+	}
 }
 
 void		kf_preview_rotate_yaw_l(void *param)
@@ -28,6 +38,16 @@ void		kf_preview_rotate_yaw_l(void *param)
 	t_vec4d	rot;
 
 	e = param;
-	rot = quat_rotator((t_vec3d){{1, 0, 0}}, -ROTATE_SPEED);
-	e->editor.item_rotation = quat_multiply(rot, e->editor.item_rotation);
+	if (!e->editor.item_placer)
+		return ;
+	if (e->editor.is_modified)
+	{
+		e->editor.item_placer->matrix = mat4_translate(
+			e->editor.item_placer->matrix, -ROTATE_SPEED, 0, 0);
+	}
+	else
+	{
+		rot = quat_rotator((t_vec3d){{1, 0, 0}}, -ROTATE_SPEED);
+		e->editor.item_rotation = quat_multiply(rot, e->editor.item_rotation);
+	}
 }
