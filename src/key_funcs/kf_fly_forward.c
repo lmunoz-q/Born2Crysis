d0fb59ae3abc8d89ef7bc79c3d39bb45c63954c0
Marek Fischer <marek5886@hotmail.com>
2019-09-03 00:08:48 +0200

    you can now move using the movement keys by pressing shift

--- src/key_funcs/kf_fly_forward.c
+++ src/key_funcs/kf_fly_forward.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/14 18:19:31 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/18 11:58:50 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/09/03 00:04:46 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@ void	kf_fly_forward(void *param)
 	t_e		*e;
 
 	e = param;
-	e->editor.editor_cam.pos = vec3vec3_substract(e->editor.editor_cam.pos,
+	if (e->editor.item_placer && e->editor.is_modified && e->input_map.keys[SDL_SCANCODE_LSHIFT].active)
+	{
+		e->editor.item_placer->matrix = mat4_translate(
+			e->editor.item_placer->matrix, 0, 0, ROTATE_SPEED);
+	}
+	else
+	{
+		e->editor.editor_cam.pos = vec3vec3_substract(e->editor.editor_cam.pos,
 		vec3scalar_multiply(e->editor.editor_cam.view_dir, FLY_SPEED));
+	}
 }
--- src/key_funcs/kf_fly_right.c
+++ src/key_funcs/kf_fly_right.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/18 12:05:05 by mfischer          #+#    #+#             */
-/*   Updated: 2019/08/18 12:05:29 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/09/03 00:06:39 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,15 @@ void	kf_fly_right(void *param)
 	t_e		*e;
 
 	e = param;
-	e->editor.editor_cam.pos = vec3vec3_substract(e->editor.editor_cam.pos,
-		vec3scalar_multiply(vec3_normalize(vec3vec3_crossproduct(e->editor.
-		editor_cam.view_dir, (t_vec3d){.a = {0, 1, 0}})), FLY_SPEED));
+	if (e->editor.item_placer && e->editor.is_modified && e->input_map.keys[SDL_SCANCODE_LSHIFT].active)
+	{
+		e->editor.item_placer->matrix = mat4_translate(
+			e->editor.item_placer->matrix, ROTATE_SPEED, 0, 0);
+	}
+	else
+	{
+		e->editor.editor_cam.pos = vec3vec3_substract(e->editor.editor_cam.pos,
+			vec3scalar_multiply(vec3_normalize(vec3vec3_crossproduct(e->editor.
+			editor_cam.view_dir, (t_vec3d){.a = {0, 1, 0}})), FLY_SPEED));
+	}
 }
