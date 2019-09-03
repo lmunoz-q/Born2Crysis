bce431a9ce656c5d1e5564303b51d1dad2ee9a88
Marek Fischer <marek5886@hotmail.com>
2019-09-03 13:37:51 +0200

    physics is now updated when the item is scaled

--- src/key_funcs/kf_scale_item.c
+++ src/key_funcs/kf_scale_item.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/18 13:43:11 by mfischer          #+#    #+#             */
-/*   Updated: 2019/09/02 23:19:39 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/09/03 13:28:57 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,9 @@ void	kf_scale_item(void *param)
 			e->editor.item_placer->matrix = mat4_scale(e->
 				editor.item_placer->matrix,
 				0.9, 0.9, 0.9);
+		mesh_delete_physics(e->editor.item_placer);
+		mesh_add_physics(e->editor.item_placer,
+		(e->editor.wall_friction_is_auto) ? -1 : e->editor.wall_friction);
 	}
 	else
 	{
