571b10c2fb85dbf08816156995f019753e6b35bb
Marek Fischer <marek5886@hotmail.com>
2019-09-03 19:02:29 +0200

    points

--- src/perlin/gen_perlin.c
+++ src/perlin/gen_perlin.c
@@ -6,17 +6,31 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 16:14:37 by mfischer          #+#    #+#             */
-/*   Updated: 2019/09/03 18:43:56 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/09/03 18:54:41 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "perlin.h"
 
-double		*gen_perlin(t_vec2i	size, double grid_ratio, int seed)
+static void	gen_points(double *points, int size)
+{
+	
+}
+
+double		*gen_perlin(t_vec2i	size, int chunk_size, int seed)
 {
 	double		*res;
+	double		*points;
 
 	if (!(res = (double *)mf_memalloc(sizeof(double) * size.n.x * size.n.y)))
 		return (NULL);
+	if (!(points = (double *)mf_memalloc(sizeof(double)
+		* ((size.n.x * size.n.y) / chunk_size))))
+	{
+		free(res);
+		return (NULL);
+	}
 	mf_seed(seed);
+	gen_points(points, (size.n.x * size.n.y) / chunk_size);
+
 }
