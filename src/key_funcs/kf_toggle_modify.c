5a0b02c64a678d008bf836b32f535011fd644894
Marek FISCHER <mfischer@e1r12p17.42.fr>
2019-09-06 13:52:14 +0200

    fix

--- src/key_funcs/kf_toggle_modify.c
+++ src/key_funcs/kf_toggle_modify.c
@@ -6,7 +6,7 @@
 /*   By: mfischer <mfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 20:36:10 by mfischer          #+#    #+#             */
-/*   Updated: 2019/09/02 20:52:40 by mfischer         ###   ########.fr       */
+/*   Updated: 2019/09/06 13:38:31 by mfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@ void	kf_toggle_modify(void *param)
 	if (e->editor.is_modified)
 		e->editor.is_modified = FALSE;
 	else
+	{
 		e->editor.is_modified = TRUE;
-	e->editor.item_placer = NULL;
+		if (e->editor.item_placer)
+		{
+			free(e->editor.item_placer->polygons);
+			mesh_delete_physics(e->editor.item_placer);
+			free(e->editor.item_placer);
+			e->editor.item_placer = NULL;
+		}
+	}
 }
