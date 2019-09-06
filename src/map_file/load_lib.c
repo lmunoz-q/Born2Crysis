5fe5f24474a1ec40826ce7460928216d25d29f7b
LIMachi <lee.mach.hm@gmail.com>
2019-09-06 11:07:04 +0200

    ajouté magic number, normé map_load

---
+++ src/map_file/load_lib.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   save_map_file.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmunoz-q <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/31 22:43:24 by lmunoz-q          #+#    #+#             */
+/*   Updated: 2019/08/31 22:43:26 by lmunoz-q         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <map_file.h>
+#include <bmml_functions.h>
+
+static inline Uint8	*load_lib_1(Uint8 *ptr, t_library *lib, t_function func)
+{
+	uint64_t	i;
+
+	i = (uint64_t)-1;
+	while (++i < func.alias_size)
+		func.alias_memory[i] = (t_entry){.type = *ptr++, .data = {NULL}};
+	i = (uint64_t)-1;
+	while (++i < func.needed_symbols)
+	{
+		func.symbols[i] = (t_symbol_data){.name = strdup((char*)ptr),
+			.ptr = NULL};
+		if (func.symbols[i].name == NULL)
+		{
+			free(lib->function_name[lib->nb_functions]);
+			free(func.symbols);
+			free(func.alias_memory);
+			free(func.code);
+			return (NULL);
+		}
+		ptr += 12;
+	}
+	lib->function[lib->nb_functions++] = func;
+	return (ptr);
+}
+
+static inline Uint8	*load_lib_0(Uint8 *ptr, t_library *lib)
+{
+	t_function	func;
+
+	func = (t_function){.code_size = 0};
+	if ((lib->function_name[lib->nb_functions] = mf_strdup((char*)ptr))
+		== NULL)
+		return (NULL);
+	ptr += 12;
+	mf_memcpy(&func, ptr, 24);
+	ptr += 24;
+	if ((func.symbols = malloc(sizeof(t_symbol_data) * func.needed_symbols))
+		== NULL
+		|| (func.alias_memory = malloc(sizeof(t_entry) * func.alias_size))
+		== NULL
+		|| (func.code = malloc(sizeof(char) * func.code_size)) == NULL)
+	{
+		free(lib->function_name[lib->nb_functions]);
+		free(func.symbols);
+		free(func.alias_memory);
+		return (NULL);
+	}
+	mf_memcpy(func.code, ptr, func.code_size);
+	ptr += func.code_size;
+	return (load_lib_1(ptr, lib, func));
+}
+
+Uint8				*load_lib(Uint8 *ptr, t_library *lib, uint64_t c)
+{
+
+
+	if ((lib->function = realloc_f(lib->function,
+			(lib->nb_functions + c) * sizeof(t_function))) == NULL)
+		return (NULL);
+	if ((lib->function_name = realloc_f(lib->function_name,
+		(lib->nb_functions + c) * sizeof(char*))) == NULL)
+	{
+		free(lib->function);
+		lib->function = NULL;
+		return (NULL);
+	}
+	while (c--)
+		ptr = load_lib_0(ptr, lib);
+	return (ptr);
+}
